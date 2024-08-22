e608cd27485b7057361f33799026e34f35c4b30f
Aung Naing Oo <aungaung20.ab@gmail.com>
2024-08-23 02:27:55 +0630

    convert to top-level statement

--- AngularLibraryHelper/Program.cs
+++ AngularLibraryHelper/Program.cs
@@ -1,44 +1,38 @@
 ﻿using System.Text;
 
-class GeneratePublicApi
+// Set the directory path of your Angular library project
+Console.Write("Enter the file path: ");
+string? projectDir = Console.ReadLine();
+
+// Create a StringBuilder to build the public-api.ts content
+StringBuilder content = new();
+
+// Iterate through the directories and subdirectories
+IterateThroughDirectories(projectDir ?? "", content, "");
+
+// Write the content to the public-api.ts file
+string publicApiPath = Path.Combine(projectDir ?? "", "index.ts");
+File.WriteAllText(publicApiPath, content.ToString());
+
+Console.WriteLine($"Generated index.ts file at {publicApiPath} use in public-api.ts");
+
+static void IterateThroughDirectories(string dir, StringBuilder content, string prefix)
 {
-    static void Main(string[] args)
+    // Get the directories and files in the current directory
+    string[] dirs = Directory.GetDirectories(dir);
+    string[] files = Directory.GetFiles(dir, "*.ts");
+
+    // Iterate through the files
+    foreach (string file in files)
     {
-        // Set the directory path of your Angular library project
-        Console.Write("Enter the file path: ");
-        string? projectDir = Console.ReadLine();
-
-        // Create a StringBuilder to build the public-api.ts content
-        StringBuilder content = new StringBuilder();
-
-        // Iterate through the directories and subdirectories
-        IterateThroughDirectories(projectDir ?? "", content, "");
-
-        // Write the content to the public-api.ts file
-        string publicApiPath = Path.Combine(projectDir ?? "", "index.ts");
-        File.WriteAllText(publicApiPath, content.ToString());
-
-        Console.WriteLine($"Generated index.ts file at {publicApiPath} use in public-api.ts");
+        string fileName = Path.GetFileNameWithoutExtension(file);
+        _ = content.AppendLine($"export * from './{prefix}{fileName}';");
     }
 
-    static void IterateThroughDirectories(string dir, StringBuilder content, string prefix)
+    // Iterate through the subdirectories
+    foreach (string subDir in dirs)
     {
-        // Get the directories and files in the current directory
-        string[] dirs = Directory.GetDirectories(dir);
-        string[] files = Directory.GetFiles(dir, "*.ts");
-
-        // Iterate through the files
-        foreach (string file in files)
-        {
-            string fileName = Path.GetFileNameWithoutExtension(file);
-            content.AppendLine($"export * from './{prefix}{fileName}';");
-        }
-
-        // Iterate through the subdirectories
-        foreach (string subDir in dirs)
-        {
-            string subDirName = Path.GetFileName(subDir);
-            IterateThroughDirectories(subDir, content, prefix + subDirName + "/");
-        }
+        string subDirName = Path.GetFileName(subDir);
+        IterateThroughDirectories(subDir, content, prefix + subDirName + "/");
     }
 }
